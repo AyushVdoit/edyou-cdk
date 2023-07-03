2070dc4b77030a03f06e33b517710a3ad75d7910
AyushVdoit <ayushjaiswal.vdoit@gmail.com>
2023-07-03 16:10:52 +0530

    1

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from edyou_cdk.edyou_cdk_stack import EdyouCdkStack
 
 
 app = cdk.App()
-EdyouCdkStack(app, "EdyouCdkStack",
+EdyouCdkStack(app, "EdyouCdkStack2",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
